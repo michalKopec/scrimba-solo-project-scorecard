35916a8c54dd57d95129700e5d1c24677b0ae78c
michalKopec <michal.apple@gmail.com>
2025-09-17 22:27:25 -0700

    Added reset button

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function updateScore(who, score) {
     let scoreEl = document.getElementById(`${who}-score`);
     scoreEl.textContent = +scoreEl.textContent + score; //convert to a number using unary +
 }
+
+function reset() {
+    document.getElementById("home-score").textContent = 0;
+    document.getElementById("guest-score").textContent = 0;
+}
